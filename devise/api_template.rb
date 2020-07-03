337ce07b56dec418a154ee07c54072ccf34ecc16
Evan Medrano <evan.a.medrano@gmail.com>
2020-07-03 07:58:58 -0400

    Update api template to include devise gem

--- devise/api_template.rb
+++ devise/api_template.rb
@@ -1,16 +1,17 @@
 require_relative "./template"
 
 def add_devise_api_template
-  add_devise_jwt_gem
+  add_devise_api_gems
   install_devise
   generate_devise_user_migration
   setup_development_environment
 end
 
 
-def add_devise_jwt_gem
+def add_devise_api_gems
   inject_into_file "Gemfile", after: "'active_model_serializers'\n" do
       <<-RUBY
+gem 'devise'
 gem 'devise-jwt'
       RUBY
     end
